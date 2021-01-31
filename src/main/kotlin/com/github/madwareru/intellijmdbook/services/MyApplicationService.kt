1872372759a0602e0619a39f34e63fb77c50a21a
GitHub Action <action@github.com>
2021-01-31 06:18:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/madwareru/intellijmdbook/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/madwareru/intellijmdbook/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.madwareru.intellijmdbook.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.madwareru.intellijmdbook.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/madwareru/intellijmdbook/services/MyProjectService.kt
+++ src/main/kotlin/com/github/madwareru/intellijmdbook/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.madwareru.intellijmdbook.services
 
+import com.github.madwareru.intellijmdbook.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
